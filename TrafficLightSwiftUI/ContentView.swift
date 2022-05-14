93165dfa1939528cb9cad64575b682e2a41b42e5
Dmitry Knauer <knauerdm@mail.ru>
2022-05-14 19:48:30 +0800

    Visual part is completed

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                CircleView(color: .red)
+                CircleView(color: .yellow)
+                CircleView(color: .green)
+                Spacer()
+                Button(action: {}) {
+                    Text("NEXT")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(width: 150.0, height: 50.0)
+                        .background(.blue)
+                        .cornerRadius(30)
+                        .overlay(RoundedRectangle(cornerRadius: 20)
+                            .stroke(.white, lineWidth: 4))
+                }
+            }
+            .padding(.bottom, 100)
+        }
     }
 }
 
