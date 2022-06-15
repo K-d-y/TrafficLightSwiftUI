6a269362f260d731204bcf1614ef8d91a1356f4d
Dmitry Knauer <knauerdm@mail.ru>
2022-06-15 13:18:30 +0800

    Bug fix. Ready app

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -7,32 +7,67 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 struct ContentView: View {
+    
+    @State private var buttonTitle = "START"
+    
+    @State private var redLightState = 0.3
+    @State private var yellowLightState = 0.3
+    @State private var greenLightState = 0.3
+    
+    @State private var currentLight = CurrentLight.red
+    
+    private func nextColor() {
+        
+        let lightIsOn = 1.0
+        let lightIsOff = 0.3
+        
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            greenLightState = lightIsOff
+            redLightState = lightIsOn
+        case .yellow:
+            currentLight = .green
+            redLightState = lightIsOff
+            yellowLightState = lightIsOn
+        case .green:
+            currentLight = .red
+            greenLightState = lightIsOn
+            yellowLightState = lightIsOff
+        }
+    }
+}
+
+extension ContentView {
     var body: some View {
         ZStack {
             Color.black
                 .ignoresSafeArea()
-            VStack {
-                CircleView(color: .red)
-                CircleView(color: .yellow)
-                CircleView(color: .green)
+            
+            VStack(spacing: 20) {
+                ColorLight(color: .red, opacity: redLightState)
+                ColorLight(color: .yellow, opacity: yellowLightState)
+                ColorLight(color: .green, opacity: greenLightState)
+                
                 Spacer()
-                Button(action: {}) {
-                    Text("NEXT")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(width: 150.0, height: 50.0)
-                        .background(.blue)
-                        .cornerRadius(30)
-                        .overlay(RoundedRectangle(cornerRadius: 20)
-                            .stroke(.white, lineWidth: 4))
+                
+                ChangeColorButton(title: buttonTitle) {
+                    if buttonTitle == "START" {
+                        buttonTitle = "NEXT"
+                    }
+                    nextColor()
                 }
             }
-            .padding(.bottom, 100)
+            .padding()
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
